722ec9a3a4794c14d3bd77404d398347b84bc404
Arthur Bufalo <hi@arthurbr.me>
2025-04-03 19:56:44 -0300

    Update hiarthurbr.nix

--- hiarthurbr.nix
+++ hiarthurbr.nix
@@ -67,7 +67,7 @@
 
     shellAliases = {
       zed = "zeditor";
-      rebuild = "sudo nixos-rebuild switch --show-trace --flake git+https://github.com/hiarthurbr/nix";
+      rebuild = "sudo nixos-rebuild switch --show-trace --flake git+https://github.com/hiarthurbr/nix --refresh";
       update = "nix-channel --update";
       upgrade = "nix-env --upgrade";
       push = "git push -u (git remote show) ((git branch --no-color | lines | where (str starts-with '*')).0 | str trim -c '*' | str trim)";
