581663db9463feb853448723e020d40cad3c39ee
Arthur Bufalo <hi@arthurbr.me>
2025-06-05 00:29:33 -0300

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -110,7 +110,7 @@
     isNormalUser = true;
     shell = pkgs.nushell;
     description = "Arthur Bufalo Rodrigues";
-    extraGroups = [ "networkmanager" "wheel" "input" "plugdev" ];
+    extraGroups = [ "networkmanager" "wheel" "input" "leds" ];
     packages = with pkgs; [];
   };
 
