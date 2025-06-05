fe47f97dad676d9a41c16e1915ecd2961b8f4392
Arthur Bufalo <hi@arthurbr.me>
2025-06-05 00:58:19 -0300

    Update global-packages.nix

--- global-packages.nix
+++ global-packages.nix
@@ -1,4 +1,7 @@
 { pkgs, ... }:
 with pkgs; [
   vim git wget curl htop btop bat
+  ripgrep fd bat interception-tools
+  interception-tools-plugins.caps2esc
+  fselect uutils-coreutils-noprefix sudo-rs mprocs
 ]
