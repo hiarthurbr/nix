fdb25dbcf910be27120690f0ac60687992af52d8
Arthur Bufalo <hi@arthurbr.me>
2025-06-08 13:21:28 -0300

    Update hiarthurbr.nix

--- hiarthurbr.nix
+++ hiarthurbr.nix
@@ -75,85 +75,10 @@
       # update = "nix-channel --update";
       # upgrade = "nix-env --upgrade";
       push = "git push -u (git remote show) ((git branch --no-color | lines | where (str starts-with '*')).0 | str trim -c '*' | str trim)";
+      cleanup = "sudo nix-env --delete-generations 7d; sudo nix-collect-garbage -d";
     };
 
-    configFile.text = ''
-      $env.config.show_banner = false;
-
-      def update [] {
-        let _old_cwd = $env.PWD
-        cd /etc/nixos
-        sudo nix flake update
-        nix-channel --update
-        nix-env --upgrade
-        cd $_old_cwd
-      }
-
-      def commit-all [message: string] {
-        git add .
-        git commit -S -m $message
-      }
-
-      # Define a function to open a project in a nix-shell environment.
-      def open-project [project: string] {
-        let project_path = $"~/Developer/($project)"
-        let devenv_path = ($project_path | path join "devenv.json")
-
-        if ($devenv_path | path exists) {
-          let devenv = open ($devenv_path | path expand)
-          let packages = ($devenv.packages | each {|p| ["-p", $p] } | flatten)
-          let editor_cmd = $devenv.editor
-          let aliases = ($devenv.commands
-                        | transpose key value
-                        | each {|entry| ($"alias ($entry.key) = ($entry.value)") })
-
-          # Build functions from the `functions` field if present
-          let functions = if (($devenv | get functions?) != null) {
-            ($devenv.functions
-              | transpose key value
-              | each {|entry|
-                  let cmd = ($entry.value | get 1)
-                  # Generate parameter names as _1, _2, etc.
-                  let param_names = $entry.value | get 0 | enumerate | each {|i| ($"_($i.index + 1): ($i.item)")} | str join ", ";
-                  ($"def ($entry.key) [($param_names)] { ($cmd) }")
-              })
-          } else {
-            ""
-          }
-
-          print ($aliases | wrap "Aliases")
-          print ($functions | wrap "Functions")
-
-          nix-shell --show-trace -p ...$packages "--command" $"nu -e 'cd ($project_path); run-external ($editor_cmd) ($project_path | path expand); ($aliases | str join '; '); ($functions | str join '; ')'"
-        } else {
-          if ($project_path | path expand | path exists) {
-            print $"Error: devenv.json not found in `($project_path)`, creating one..."
-            ({
-              "$schema": "https://gist.githubusercontent.com/hiarthurbr/70943959bc52a3dd8afd79a3f9e79128/raw/nu-open-project-devenv-schema.json",
-              "packages": ["bun", "biome", "nodejs_18"],
-              "editor": "zeditor",
-              "commands": {
-                "install": "bun install",
-                "dev": "bun run dev"
-              },
-              "functions": {
-                "start": [[], "bun run build; bun run -b start"],
-                "add": [["string"], "bun run add $_1"]
-              }
-            }) | save -p $devenv_path
-            print "Done!"
-          } else { print $"Error: `($project_path)` does not exist." }
-        }
-      }
-
-      alias op = open-project
-
-      const starship_config = $nu.data-dir | path join "vendor/autoload"
-      if (not (($starship_config | path join "starship.nu") | path exists)) {
-        mkdir $starship_config
-        starship init nu | save -f ($nu.data-dir | path join ($starship_config | path join "starship.nu"))
-      }
-    '';
+    configFile.source = ./config.nu;
   };
 
   programs.git = {
