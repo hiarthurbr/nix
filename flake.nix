8ffdae40b036f8ba0bf8c21ed2295f170ea30629
hiarthurbr <hi@arthurbr.me>
2025-08-26 22:09:53 -0300

    chore: flake update 2025-08-26 22:09:53

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
-    unstable.url = "github:nixos/nixpkgs/nixos-unstable"
+    unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = { 
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
