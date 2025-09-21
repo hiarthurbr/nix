b865f4f9b0fa04b7df14720fd57114bde9d6bfb1
hiarthurbr <hi@arthurbr.me>
2025-09-21 19:48:58 -0300

    chore: Config update 2025-09-21 19:48:58

--- configuration.nix
+++ configuration.nix
@@ -113,33 +113,34 @@
     # no need to redefine it in your config for now)
     #media-session.enable = true;
 
-    extraConfig.pipewire.vban = {
-      context.modules = [
-        {
-          name = "libpipewire-module-vban-send";
-          args = {
-            #local.ifname = "eth0"
-            #source.ip = "0.0.0.0"
-            destination.ip = "10.0.0.2";
-            destination.port = 6980;
-            #net.mtu = 1500
-            #net.ttl = 1
-            #net.loop = false
-            #sess.min-ptime = 2
-            #sess.max-ptime = 20
-            #sess.name = "PipeWire VBAN stream"
-            #sess.media = "audio"
-            #audio.format = "S16LE"
-            #audio.rate = 44100
-            #audio.channels = 2
-            #audio.position = [ FL FR ]
-            stream.props = {
-              node.name = "nixos";
-            };
-          };
-        }
-      ];
-    };
+    # extraConfig.pipewire.vban = {
+    #   context.modules = [
+    #     {
+    #       name = "libpipewire-module-vban-send";
+    #       args = {
+    #         #local.ifname = "eth0"
+    #         #source.ip = "0.0.0.0"
+    #         destination.ip = "10.0.0.2";
+    #         destination.port = 6980;
+    #         #net.mtu = 1500
+    #         #net.ttl = 1
+    #         #net.loop = false
+    #         #sess.min-ptime = 2
+    #         #sess.max-ptime = 20
+    #         #sess.name = "PipeWire VBAN stream"
+    #         #sess.media = "audio"
+    #         #audio.format = "S16LE"
+    #         #audio.rate = 44100
+    #         #audio.channels = 2
+    #         #audio.position = [ FL FR ]
+    #         stream.props = {
+    #           node.name = "nixos";
+    #         };
+    #       };
+    #     }
+    #   ];
+    # };
+    extraConfig.pipewire.vban = ./config.nu;
   };
 
   # Enable touchpad support (enabled default in most desktopManager).
