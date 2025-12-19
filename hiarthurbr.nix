6768459e38feda87990d7adbeb0eb44a9a071fc8
hiarthurbr <hi@arthurbr.me>
2025-12-18 23:54:28 -0300

    chore: Config update 2025-12-18 23:54:28

--- hiarthurbr.nix
+++ hiarthurbr.nix
@@ -15,7 +15,7 @@
     fish nushell
 
     # Shell stuff
-    eza zoxide xh zellij dust dua yazi starship hyfetch
+    eza zoxide xh zellij dust dua yazi starship hyfetch fastfetch
 
     # Git stuff
     git-cliff jujutsu gitui
@@ -70,6 +70,108 @@
       init.defaultBranch = "master";
       gpg.format = "ssh";
       user.signingkey = "/home/hiarthurbr/.ssh/id_ed25519.pub";
+    };
+  };
+
+  dconf = {
+    enable = true;
+
+    settings = {
+
+      "org/gnome/shell" = {
+        disable-user-extensions = false;
+        enabled-extensions = with pkgs.gnomeExtensions; [
+          blur-my-shell.extensionUuid
+          just-perfection.extensionUuid
+          arc-menu.extensionUuid
+          appindicator.extensionUuid
+          dash-to-dock.extensionUuid
+          applications-menu.extensionUuid
+        ];
+        favorite-apps = [
+          "helium.desktop"
+          "org.gnome.Nautilus.desktop"
+          "com.mitchellh.ghostty.desktop"
+          "dev.zed.Zed.desktop"
+          "proton-pass.desktop"
+        ];
+      };
+
+      "org/gnome/desktop/interface".color-scheme = "prefer-dark";
+
+      "org/gnome/shell/extensions/blur-my-shell" = {
+        hacks-level = 1;
+
+        appfolder = {
+          brightness = 0.6;
+          sigma = 30;
+        };
+
+        applications = {
+          blur = true;
+          blur-on-overview = true;
+          brightness = 1.0;
+          dynamic-opacity = false;
+          enable-all = true;
+          opacity = 190;
+          sigma = 30;
+        };
+
+        "dash-to-dock" = {
+          blur = true;
+          brightness = 1.0;
+          override-background = true;
+          pipeline = "pipeline_default";
+          sigma = 6;
+          static-blur = false;
+          style-dash-to-dock = 0;
+          unblur-in-overview = true;
+        };
+
+        "dash-to-panel".blur-original-panel = true;
+
+        hidetopbar.compatibility = false;
+
+        lockscreen.pipeline = "pipeline_default";
+
+        overview = {
+          pipeline = "pipeline_default";
+          style-components = 3;
+        };
+
+        panel = {
+          brightness = 0.6;
+          pipeline = "pipeline_default";
+          sigma = 30;
+          static-blur = true;
+        };
+
+        screenshot.pipeline = "pipeline_default";
+
+        window-list = {
+          brightness = 0.6;
+          sigma = 30;
+        };
+      };
+
+      "org/gnome/shell/extensions/dash-to-dock": {
+        apply-custom-theme = false;
+        background-color = 'rgb(0,0,0)';
+        background-opacity = 0.37;
+        custom-background-color=true;
+        custom-theme-shrink=false;
+        customize-alphas=true;
+        dash-max-icon-size=48;
+        dock-position='BOTTOM';
+        height-fraction=0.90000000000000002;
+        intellihide-mode='FOCUS_APPLICATION_WINDOWS';
+        max-alpha=0.40000000000000002;
+        min-alpha=0.29999999999999999;
+        preferred-monitor=-2;
+        preferred-monitor-by-connector='HDMI-1';
+        require-pressure-to-show=false;
+        transparency-mode='DYNAMIC';
+      };
     };
   };
 }
