bf7142fabf85b6f711de91df77431bbc48dfa172
hiarthurbr <hi@arthurbr.me>
2025-12-21 23:55:59 -0300

    chore: Flake update 2025-12-21 23:55:58

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "flake for hiarthurbr with Home Manager enabled";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
