1f1d2996ebcb2f7cf27bbf5f48d5f40ffc675bb2
hiarthurbr <hi@arthurbr.me>
2026-07-27 23:49:23 -0300

    chore: Config update 2026-07-27 23:49:23

--- configuration.nix
+++ configuration.nix
@@ -59,6 +59,9 @@
   # Configure console keymap
   console.keyMap = "br-abnt2";
 
+  systemd.services.systemd-rfkill.enable = false;
+  systemd.services.systemd-rfkill.socket.enable = false;
+
   services = {
     # Enable the X11 windowing system.
     # xserver.enable = true;
